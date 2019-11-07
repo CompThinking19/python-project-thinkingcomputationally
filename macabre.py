54252313cbad80414e1792df4cf141775f7d47b3
thinkingcomputationally <55244679+thinkingcomputationally@users.noreply.github.com>
2019-11-07 12:03:28 -0500

    In Class Work 11/7

--- macabre.py
+++ macabre.py
@@ -11,25 +11,33 @@ import os
 
 open_table = open("mortality_table.csv")
 read_the_file = csv.reader(open_table)
-for item in read_the_file:
-    print item
+# for item in read_the_file:
+
+    #print item
 
 sex = raw_input("Are you male or female?")
+# inputs must be 'male', 'Male', 'female', 'Female'
+# allow capatalized inputs
+    # raise TypeError
+        # if sex =! str
 print sex
 
 age = raw_input("How old are you?")
+#     raise TypeError
+#         if age =! int
 print age
+# life_expectancy = age, sex in the mortality_table
+# create multiple choice factors
+zero_once_a_week = 0
+drink = raw_input("How often do you drink? Zero - Once a week, etc.... ")
+# input drink
+
+life_expectancy = life_expectancy + zero_once_a_week
 
 
 
-#if raw_input =
-    #return item
-#     fieldnames = []
-#     age == 0
-# def factors (age, gender)
-#     for row in csv.reader
-#         if age == 0 and gender = 0
-#         return fieldname
+
+
 
 #coding that needs to be completed:
 #simplify the mortality_table (get rid of death probability)
