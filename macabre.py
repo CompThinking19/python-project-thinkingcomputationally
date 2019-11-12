566be25cbf38b3a59ff056ed4d7445a655e6921b
thinkingcomputationally <55244679+thinkingcomputationally@users.noreply.github.com>
2019-11-11 23:49:59 -0500

    working on creating basic functions/dictionaries

--- macabre.py
+++ macabre.py
@@ -12,30 +12,43 @@ import os
 open_table = open("mortality_table.csv")
 read_the_file = csv.reader(open_table)
 # for item in read_the_file:
-
-    #print item
+#print item
 
 sex = raw_input("Are you male or female?")
 # inputs must be 'male', 'Male', 'female', 'Female'
 # allow capatalized inputs
-    # raise TypeError
-        # if sex =! str
+# raise TypeError
+# if sex =! str
 print sex
 
 age = raw_input("How old are you?")
-#     raise TypeError
-#         if age =! int
+# raise TypeError if age =! int
 print age
 # life_expectancy = age, sex in the mortality_table
 # create multiple choice factors
-zero_once_a_week = 0
-drink = raw_input("How often do you drink? Zero - Once a week, etc.... ")
-# input drink
+data = []
+for row in reader
+    row = [all_ages, male_life_expectancy, female_life_expectancy]
+    all_ages = float(row[0])
+    male life expectancy = float(row[1])
+    female life expectancy = float(row[2])
 
-life_expectancy = life_expectancy + zero_once_a_week
+    if sex = male_life_expectancy and age = all_ages
+        data.append(row)
+    elif sex = female_life_expectancy
+        data.append(row)
+    return data
 
-
-
+drink = raw_input("How often do you drink? A) Zero - Once a week, B) Twice Several times a week C) Once daily D) Several times a day.... ")
+    if drink = "A":
+        data = data - 0
+    elif drink = "B"
+        data = data - 2
+    elif drink = "C"
+        data = data - 5
+    elif drink = "D"
+        data = data - 10
+    else "You typed an invalid answer. Please try again."
 
 
 
