a8b8335e56621b5a13d47b81923753cd8e4a3805
thinkingcomputationally <55244679+thinkingcomputationally@users.noreply.github.com>
2019-11-27 19:16:27 -0500

    project completed, but terminal does not work on my end

---
+++ README.txt
@@ -0,0 +1,26 @@
+Macabre was created to determine one's life expectancy based on his lifestyle choices.
+The most common factors in other calculators include age, biological sex, drinking habits, and smoking habits.
+Sometimes we are unaware that we are living on borrowed time.
+I created a death calculator to remind users to make better choices and to keep in mind that this life isn't forever,
+so make the best out of the time you have left.
+
+I would have liked to include height and weight (BMI), diet, and exercise,
+but I could not find substantial information to include in my calculator.
+I didn't want to deal with studies that I couldn't interpret
+nor had the best information to incorporate into my calculator.
+
+Here's how it is supposed to work:
+The CSV includes life expectancy data for females and males, according to current age.
+The CSV reader aligns the sex and age according to the user's answers.
+It then adds or substracts years off of the life expectancy age, according to the answers that the user gives
+for the health questions.
+At the end of the calculator, it gives the user his estimated life expectancy.
+If my terminal had been working, I would have also like to estimate the year that the user might die.
+
+One of my biggest issues with this project was running the code. I cannot remember how PowerShell works,
+and all the terminals that I have downloaded in Atom either did not work or could only run code written for Python 3.
+I started converting my code to Python 3 (raw_input = input, print = print(), et cetera).
+However, I kept running into issues that I could not solve on my own and converted everything back.
+
+Overall, I wish I could have figured out how to work terminal because I can't tell what it looks likes
+or if it is able to do wat I want it to do.
--- macabre.py
+++ macabre.py
@@ -1,85 +1,96 @@
 # load the mortality table by importing csv
 import csv
-# import pandas as import pdb; pdb.set_trace()
 import os
 
-print """Welcome to Macabre, your own personalized death calculator.
-Today we will be calculating your life expectancy and/or probable year of death.
+print
+"""
+Welcome to Macabre, your own personalized death calculator.
+Today we will be calculating your life expectancy.
 DISCLAIMER: RESULTS PRESENTED IN MACABRE ARE NOT DEFINITIE.
 MACARBE CANNOT FACTOR FATAL OCCURENCES OR TERMINAL ILLNESSES.
-USE AT YOUR OWN RISK."""
+USE AT YOUR OWN RISK.
+"""
 
-#opens mortality table
+#opens mortality table in csv
 open_table = open("mortality_table.csv")
 read_the_file = csv.reader(open_table)
 
 #use string to enter age and gender
 sex = raw_input("Are you male or female?")
-    if raw_input == "male" or "female":
-        print sex.lower()
-    else:
-        print "Invalid input"
+if raw_input == "male" or "female":
+    print sex.lower
+else:
+    print "Invalid input"
 
 age = int(raw_input("How old are you?"))
 print age
 
-#set variables for the possible values in the mortality table
+#set variables for the possible values in the mortality table for male and female life expectancies
+#data holds this information
 data = []
-for row in reader
+for row in reader:
     male_life_expectancy = float(row[1])
     female_life_expectancy = float(row[2])
     topics = [male_life_expectancy, female_life_expectancy]
     data.append(topics)
 
+#male is assigned to variables in column 0, female is assigned to variables in column 1
 male_or_female = 0
 if sex == "female":
     male_or_female = 1
 current_life_expectancy = data[age][male_or_female]
 
-#Create a while loop if an invalid answer is input
-
+#Create a while loop if an invalid answer is input for health questions
+#selected answer subtracts years from life expectancy
 correct_answer = False
 while correct_answer == False:
-    drink = raw_input("""How often do you drink?
-    A) Zero - Once a Week
-    B) Twice Several times a Week
-    C) At Least Once Daily
-    D) Several Times a Day"""
+    drink = raw_input(
+    """
+    How many standard alcoholic drinks do you have per week?
+    A) 0 - 7
+    B) 8 - 14
+    C) 14 - 25
+    D) 25 +
+    """
+    )
 
     if drink == "A":
         correct_answer = True
     elif drink == "B":
-        current_life_expectancy -=2
+        current_life_expectancy -=0.5
         correct_answer = True
     elif drink == "C":
-        current_life_expectancy -= 5
-        correct_answer = True
-    elif drink == "D":
-        current_life_expectancy -= 10
-        correct_answer = True
-    else:
-        print "You typed an invalid answer. Please try again."
-
-smoke = raw_input("""How often do you smoke?
-A) Zero - Two Times a Week
-B) Several times a Week
-C) At Least Once Daily
-D) Several Times a Day""" )
-    if smoke == "A":
-        correct_answer = True
-    elif smoke == "B":
         current_life_expectancy -=2
         correct_answer = True
-    elif smoke == "C":
-        current_life_expectancy -= 5
-        correct_answer = True
-    elif smoke == "D":
-        current_life_expectancy -= 10
+    elif drink == "D":
+        current_life_expectancy -=5
         correct_answer = True
     else:
         print "You typed an invalid answer. Please try again."
 
-print "According to your answers, you have" current_life_expectancy "years to live."
-#alternative ending
-data = 2019 + current_life_expectancy
-print "According to your answers, you are expected to die in " current_life_expectancy"."
+smoke = raw_input(
+"""
+How many cigarettes do you smoke per day?
+A) 0
+B) less than 5
+C) 5 - 20
+D) 20 +
+"""
+)
+if smoke == "A":
+        correct_answer = True
+elif smoke == "B":
+        current_life_expectancy -=5
+        correct_answer = True
+elif smoke == "C":
+        current_life_expectancy -=9
+        correct_answer = True
+elif smoke == "D":
+        current_life_expectancy -=13
+        correct_answer = True
+else:
+    print "You typed an invalid answer. Please try again."
+
+#input final life expectancy (int) into the string
+stuff_in_string = "According to your answers, you have %d to live." % (current_life_expectancy)
+print stuff_in_string
